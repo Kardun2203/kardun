661b9bed05316e115ef2ae623c63f91d242d2f12
Kardun2203 <75862501+Kardun2203@users.noreply.github.com>
2020-12-12 05:37:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'Furukawa Bot☄'; 
+const instagram = 'https://instagram.com/furukawadensetsu'; 
+const telegram = 'https://t.me/Furukawa'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/FRKW0415';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
